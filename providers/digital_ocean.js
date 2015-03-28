66b8fd6213c56424e99502277617a149b6c3637d
Norman Joyner <norman.joyner@gmail.com>
2015-03-28 16:49:04 -0400

    add droplet_id to digital ocean metadata

--- providers/digital_ocean.js
+++ providers/digital_ocean.js
@@ -1,23 +1,42 @@
+var async = require("async");
 var request = require("request");
 
 module.exports = {
 
     is_true: function(fn){
-        var options = {
-            url: "http://169.254.169.254/metadata/v1/region",
-            method: "GET",
-            timeout: 5000
-        }
+        async.parallel({
+            droplet_id: function(fn){
+                var options = {
+                    url: "http://169.254.169.254/metadata/v1/id",
+                    method: "GET",
+                    timeout: 5000
+                }
 
-        request(options, function(err, response){
-            if(err || response.statusCode != 200)
-                return fn();
-            else{
-                return fn({
-                    provider: "digitalocean",
-                    region: response.body
+                request(options, function(err, response){
+                    if(err || response.statusCode != 200)
+                        return fn();
+                    else
+                        return fn(null, response.body);
+                });
+            },
+
+            region: function(fn){
+                var options = {
+                    url: "http://169.254.169.254/metadata/v1/region",
+                    method: "GET",
+                    timeout: 5000
+                }
+
+                request(options, function(err, response){
+                    if(err || response.statusCode != 200)
+                        return fn();
+                    else
+                        return fn(null, response.body);
                 });
             }
+        }, function(err, metadata){
+            metadata.provider = "digitalocean";
+            return fn(metadata);
         });
     }
 
